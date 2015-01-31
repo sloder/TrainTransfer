d7c97495e63a0ad18a16ffdb32d9408d935b00e0
Tomas <tom.yangc@gmail.com>
2015-02-01 00:29:47 +0800

    go

--- RailWay2015/ChangeTrains/Form1.cs
+++ RailWay2015/ChangeTrains/Form1.cs
@@ -14,5 +14,10 @@ namespace ChangeTrains
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
